c71b76261e1d6a74e7b888ddbb953f14b3ada54d
dumduque <dumduque@bancolombia.com.co>
2021-12-10 15:46:54 -0500

    secord commit angular price crypto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Coin } from './model/coin';
+import { PriceService } from './price.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-price-crypto';
+  coins: Coin[] = [];
+  coinsFiltered: Coin[] = [];
+  searchText = '';
+
+  constructor(private priceService: PriceService) {}
+
+  ngOnInit() {
+    this.priceService.getAllPrice().subscribe((res) => {
+      this.coins = res;
+      this.coinsFiltered = res;
+    });
+  }
+
+  searchCoin() {
+    this.coinsFiltered = this.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+    console.log(this.searchText);
+  }
 }
